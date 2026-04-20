1dcf48015fb78a18222cc85b0da04da4efb1680b
Your Name <juanpascualferrer@gmail.com>
2026-04-20 18:33:13 +0200

    Agregar archivos de proyecto.

---
+++ ConsoleApp1/Data.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MySqlConnector;
+
+namespace ConsoleApp1
+{
+    internal class Data
+    {
+        private string connectionString =
+            "datasource = 127.0.0.1;" +
+            "port = 3306;" +
+            "username = root; password = ;" +
+            "database = hospital";
+
+        
+        private int Insert(string query)
+        {
+            MySqlConnection connection = new MySqlConnection(connectionString);
+            MySqlCommand commandDatabase = new MySqlCommand(query, connection);
+            try
+            {
+                connection.Open();
+                int result = commandDatabase.ExecuteNonQuery();
+                return (int)commandDatabase.LastInsertedId;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            return -1;
+        }
+    }
+}
